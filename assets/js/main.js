d0b43782b134943c162c5ae39379a1bb37202ce3
Daniel Agyei <dandesign96@gmail.com>
2024-09-21 17:19:35 +0000

    Done with medium screen responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,31 @@
+const header = document.querySelector(".js-header");
 const navbar = document.querySelector(".js-nav");
 const navbarOverlay = document.querySelector(".js-nav-overlay");
 const navTogglers = document.querySelectorAll(".js-nav-toggler, .js-nav-link");
 
 Array.from(navTogglers).forEach((btn) => {
-  addClickEvent(btn, function () {
-    console.log("click");
-    navbar.classList.toggle("is-open");
-    navbarOverlay.classList.toggle("is-open");
-  });
+  addEventToElem("click", btn, navToggler);
 });
 
-/*
-    #Click Event Handler
-*/
-function addClickEvent(target, handler) {
-  target.addEventListener("click", handler);
+// scroll handler
+function stickyHeader() {
+  window.scrollY > 100
+    ? header.classList.add("is-active")
+    : header.classList.remove("is-active");
+}
+
+document.addEventListener("scroll", stickyHeader);
+
+// navbar toggler
+function navToggler() {
+  navbar.classList.toggle("is-open");
+  navbarOverlay.classList.toggle("is-open");
+}
+
+Array.from(navTogglers).forEach((btn) => {
+  addEventToElem("click", btn, navToggler);
+});
+// click event
+function addEventToElem(eventType, target, handler) {
+  target.addEventListener(eventType, handler);
 }

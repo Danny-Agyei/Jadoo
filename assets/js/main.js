2a374f4ff6f4ffc4d52c8445020963d0ceed7a53
Daniel Agyei <dandesign96@gmail.com>
2024-09-25 19:07:51 +0000

    fix hero banner preload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,29 +2,43 @@ const header = document.querySelector(".js-header");
 const navbar = document.querySelector(".js-nav");
 const navbarOverlay = document.querySelector(".js-nav-overlay");
 const navTogglers = document.querySelectorAll(".js-nav-toggler, .js-nav-link");
+const page = document.getElementsByTagName("body");
 
-Array.from(navTogglers).forEach((btn) => {
-  addEventToElem("click", btn, navToggler);
-});
-
-// scroll handler
-function stickyHeader() {
-  window.scrollY > 100
-    ? header.classList.add("is-active")
-    : header.classList.remove("is-active");
+// Reset scroll position
+function resetScrollPos() {
+  window.scrollTo(0, 0);
 }
 
-document.addEventListener("scroll", stickyHeader);
+addEventToElem("load", window, resetScrollPos);
+
+// Sticky navbar
+function stickyHeader() {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY === 0) {
+    header.classList.remove("is-active");
+  } else if (currentScrollY < lastScrollY) {
+    header.classList.add("is-active");
+  } else {
+    header.classList.remove("is-active");
+  }
+
+  lastScrollY = currentScrollY;
+}
+
+addEventToElem("scroll", window, stickyHeader);
 
 // navbar toggler
 function navToggler() {
   navbar.classList.toggle("is-open");
   navbarOverlay.classList.toggle("is-open");
+  page.classList.toggle("has-no-overflow");
 }
 
 Array.from(navTogglers).forEach((btn) => {
   addEventToElem("click", btn, navToggler);
 });
+
 // click event
 function addEventToElem(eventType, target, handler) {
   target.addEventListener(eventType, handler);
